94e5874dcb36ed924a5da33aeabba8bb33b801f3
8eel <8eel@fresnou.org>
2023-02-06 13:40:46 -0800

    initial commit

---
+++ DataTypes.js
@@ -0,0 +1,55 @@
+console.log(7);
+
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(8);
+console.log(7);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.123456);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("Hello World");
+console.log('!@#$ is also a string');
+console.log('backticks by the 1');
+
+//Arithmetic Operator (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is calling MOD
+                   //it means reminder after dividing
+
+//Concatenation mean to combine strings
+console.log("My name"+" is Arther Morgan");
+console.log('My'+' '+'is'+14+'.');
+console.log("Your age is\n14.");
+
+//Template literal - plug in calculated value
+//Use backticks '' by the 1 on your keyboard
+console.log('15 divided by 10 = ${15/10} asdfsadf')
+
+//Unary Operators use one value
+console.log(typeof 18);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+
+//Binary Operators use two values
+//Comparison Operators
+console.log(10 > 5); //Greater than
+console.log(10 < 5); //Less than
+console.log(10 >= 5); //Greater than or equal to
+console.log(10 <= 5); // Less than or equal to
+console.log(10 == 5); //are they both equal? false
+console.log(10 == 10); //are they both equal? true
+console.log(10 != 5); //are they different? true
+console.log( 10 != 10); //are they different? false
+//Logical Operators
+
